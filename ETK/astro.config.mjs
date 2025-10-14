50028af73b38e74227ca65fb932a80fa34f7ab04
DerrickMC7 <monroederrick70@gmail.com>
2025-10-14 17:41:41 -0600

    version that works for streaming online for the Real VM

--- ETK/astro.config.mjs
+++ ETK/astro.config.mjs
@@ -1,23 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
-    server: {
-  proxy: {
-    '/owncast': {
-      target: 'http://209.148.103.84:8080',
-      changeOrigin: true,
-      rewrite: (path) => path.replace(/^\/owncast/, ''),
-    },
-  },
-},
-
-  }
+    // Ya no hay proxy
+    // server: {
+    //   proxy: {
+    //     '/owncast': {
+    //       target: 'http://209.148.103.84:8080',
+    //       changeOrigin: true,
+    //       rewrite: (path) => path.replace(/^\/owncast/, ''),
+    //     },
+    //   },
+    // },
+  }
 });
-
-
